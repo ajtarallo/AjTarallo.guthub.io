73738cc7edcc17e5dce4ccd4d587203852dcb4d5
ajtarallo <57505903+ajtarallo@users.noreply.github.com>
2019-11-12 11:18:50 -0500

    Add files via upload

---
+++ BUA 235/styles.css
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+
+<html>
+
+
+h1 {color: red; font-size: 22pt;} 
+p {color: black; font-weight: bold;} 
+Body { background: maroon ; 
+	     color: black;
+	     font-size: 14pt;
+	   }
+	   H1 {font-size: 250%;
+	              font-weight: bold;
+	              color: red}
+me p { color: orange }
+
+
+       
+
+
+</html>
+
